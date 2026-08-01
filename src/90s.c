491a5ce8b5a5c011ce34c1ed822796051aa7cb65
night0721 <night@night0721.xyz>
2026-08-01 16:27:09 +0100

    Fix multiline render

--- src/90s.c
+++ src/90s.c
@@ -1,3 +1,5 @@
+#define _XOPEN_SOURCE 600
+
 #include <termios.h>
 #include <unistd.h>
 #include <stdlib.h>
@@ -8,6 +10,7 @@
 #include <stdbool.h>
 #include <signal.h>
 #include <ctype.h>
+#include <sys/ioctl.h>
 
 #include "constants.h"
 #include "history.h"
@@ -21,6 +24,13 @@ void *memalloc(size_t size)
 		exit(EXIT_FAILURE);
 	}
 	return ptr;
+}
+
+int get_terminal_width(void) {
+    struct winsize ws;
+    if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0)
+        return 80; // fallback
+    return ws.ws_col;
 }
 
 void change_terminal_attribute(int option)
@@ -105,6 +115,30 @@ void clearline(void)
 	printf("\033[K"); // clear line to the right of cursor
 }
 
+int prompt_visible_length(const char *str)
+{
+	int len = 0;
+	int in_esc = 0;
+
+	if (!str)
+		return 0;
+
+	while (*str) {
+		if (*str == '\033') {
+			in_esc = 1;
+		} else if (in_esc) {
+			/* ESC sequences typically end at a letter (e.g., 'm' in \033[34m) */
+			if ((*str >= 'A' && *str <= 'Z') || (*str >= 'a' && *str <= 'z'))
+				in_esc = 0;
+		} else {
+			len++;
+		}
+		str++;
+	}
+
+	return len;
+}
+
 void highlight(char *buffer, char **paths)
 {
 	char *cmd_part = strchr(buffer, ' ');
@@ -148,17 +182,68 @@ void highlight(char *buffer, char **paths)
 	free(command_without_arg);
 }
 
-char *readline(char **paths)
+void render(const char *prompt, const char *buffer, int cursor_pos,
+                 int *prev_lines_out, char **paths) {
+    int width = get_terminal_width();
+    int prompt_visible = prompt_visible_length(prompt); // strip ANSI for counting
+
+    // Move cursor up to the first line if we were multiline
+    if (*prev_lines_out > 1) {
+        printf("\033[%dA", *prev_lines_out - 1);  // move up
+    }
+    printf("\r");  // go to start of line
+
+    // Clear all lines we might have used
+    for (int i = 0; i < *prev_lines_out; i++) {
+        printf("\033[2K");  // clear entire line
+        if (i < *prev_lines_out - 1) printf("\n");
+    }
+
+    // Move back up to first line
+    if (*prev_lines_out > 1) {
+        printf("\033[%dA", *prev_lines_out - 1);
+    }
+
+    printf("\r");
+
+    // Print prompt + highlighted buffer
+    printf("%s", prompt);
+    highlight(buffer, paths);  // your existing highlight function
+
+    // Calculate where cursor should be
+    int total_visible = prompt_visible + cursor_pos;
+    int target_line = total_visible / width;
+    int target_col = total_visible % width;
+
+    // Move cursor to correct position
+    printf("\r");
+    if (target_line > 0) {
+        printf("\033[%dB", target_line);  // move down
+    }
+    if (target_col > 0) {
+        printf("\033[%dC", target_col);   // move right
+    }
+
+    fflush(stdout);
+
+    // Update prev_lines for next render
+    int new_total = prompt_visible + strlen(buffer);
+    *prev_lines_out = new_total / width + (new_total % width ? 1 : 0);
+    if (*prev_lines_out < 1) *prev_lines_out = 1;
+}
+
+char *readline(char **paths, char *prompt)
 {
 	int bufsize = RL_BUFSIZE;
 	int position = 0;
 	char *buffer = memalloc(bufsize);
+	buffer[0] = '\0';
 
-	bool moved = false;
-	bool backspaced = false;
-	bool navigated = false;
-	bool insertatmiddle = false;
-	bool replaced = false;
+	int prev_lines = 1;
+	int prompt_len = prompt_visible_length(prompt);
+
+	printf("%s", prompt);
+	fflush(stdout);
 
 	buffer[0] = '\0';
 	while (1) {
@@ -169,177 +254,87 @@ char *readline(char **paths)
 		switch (c) {
 			case EOF:
 				exit(EXIT_SUCCESS);
-			case 10: {
-						 // enter/new line feed
-						 if (buf_len == 0) {
-							 return NULL;
-						 }
-						 // check if command includes !!
-						 if (strstr(buffer, "!!") != NULL) {
-							 char *last_command = read_command(1);
-							 if (last_command != NULL) {
-								 // replace !! with the last command
-								 char *replace = strstr(buffer, "!!");
-								 int replace_len = strlen(replace);
-								 int last_command_len = strlen(last_command);
-								 int buffer_len = strlen(buffer);
-								 if (last_command_len > replace_len) {
-									 buffer = realloc(buffer, buffer_len + last_command_len - replace_len + 1);
-									 if (!buffer) {
-										 fprintf(stderr, "90s: Error allocating memory\n");
-										 exit(EXIT_FAILURE);
-									 }
-								 }
-								 memmove(replace + last_command_len, replace + replace_len, buffer_len - (replace - buffer) - replace_len + 1);
-								 memcpy(replace, last_command, last_command_len);
-								 position += last_command_len - replace_len;
-								 shiftright(last_command_len - replace_len);
-								 replaced = true;
-								 break;
-							 }
-						 } else {
-							 buffer[buf_len] = '\0';
-							 // clear all characters after the command
-							 for (int start = buf_len + 1; buffer[start] != '\0'; start++) {
-								 buffer[start] = '\0';
-							 }
-							 printf("\n"); // give space for response
-							 position = 0;
-							 return buffer;
-						 }
-					 }
+
+			// Enter
+			case '\n':
+				buffer[buf_len] = '\0';
+				printf("\n");
+				
+				// Check if command includes !!
+				if (strstr(buffer, "!!") != NULL) {
+					char *last_command = read_command(1);
+					if (last_command != NULL) {
+						// replace !! with the last command
+						char *pos = strstr(buffer, "!!");
+						char tmp[1024];
+						snprintf(tmp, sizeof(tmp), "%.*s%s%s",
+								 (int)(pos - buffer), buffer,
+								 last_command,
+								 pos + 2);
+						
+						/* Copy back or realloc to fit */
+						buffer = realloc(buffer, strlen(tmp) + 1);
+						strcpy(buffer, tmp);
+/* 						TODO: position += last_command_len - replace_len; */
+						break;
+					}
+				}
+				return buffer;
+
 			case 127: // backspace
-					 if (buf_len >= 1) {
-						 position--;
-						 for (int i = position; i < buf_len; i++) {
-							 // shift the buffer
-							 buffer[i] = buffer[i + 1];
-						 }
-						 backspaced = true;
-						 moved = false;
-					 }
-					 break;
-			case 27: // arrow keys comes at three characters, 27, 91, then 65-68
-					 if (getchar() == 91) {
-						 int arrow_key = getchar();
-						 if (arrow_key == 65) { // up
-												// read history file and fill prompt with latest command
-							 char *last_command = read_command(1);
-							 if (last_command != NULL) {
-								 strcpy(buffer, last_command);
-							 }
-							 navigated = true;
-							 moved = false;
-							 break;
-						 } else if (arrow_key == 66) { // down
-							 char *last_command = read_command(0);
-							 if (last_command != NULL) {
-								 strcpy(buffer, last_command);
-							 }
-							 navigated = true;
-							 moved = false;
-							 break;
-						 } else if (arrow_key == 67) { // right
-							 if (position < buf_len) {
-								 shiftright(1);
-								 position++;
-							 }
-							 moved = true;
-							 break;
-						 } else if (arrow_key == 68) { // left
-							 if (position >= 1) {
-								 shiftleft(1);
-								 position--;
-							 }
-							 moved = true;
-							 break;
-						 } 
-					 }
+				if (buf_len > 0) {
+					memmove(&buffer[position - 1], &buffer[position], buf_len - position + 1);
+					position--;
+				}
+				break;
+
+			case 27: // Arrow keys has three characters, 27, 91, then 65-68
+				if (getchar() == '[') {
+                    int arrow = getchar();
+                    if (arrow == 'A') { // Up - history
+                        char *hist = read_command(1);
+                        if (hist) {
+                            strncpy(buffer, hist, bufsize-1);
+                            buffer[bufsize-1] = '\0';
+                            position = strlen(buffer);
+                        }
+                    } else if (arrow == 'B') { // Down
+                        char *hist = read_command(0);
+                        if (hist) {
+                            strncpy(buffer, hist, bufsize-1);
+                            buffer[bufsize - 1] = '\0';
+                            position = strlen(buffer);
+                        } else {
+                            buffer[0] = '\0';
+                            position = 0;
+                        }
+                    } else if (arrow == 'C') { // Right
+                        if (position < buf_len) position++;
+                    } else if (arrow == 'D') { // Left
+                        if (position > 0) position--;
+                    }
+                }
+                break;
+
 			default:
-					 if (c > 31 && c < 127) {
-						 if (position == buf_len) {
-							 // Append character to the end of the buffer
-							 buffer[buf_len] = c;
-							 buffer[buf_len + 1] = '\0';
-							 moved = false;
-							 navigated = false;
-						 } else {
-							 // Insert character at the current position
-							 memmove(&buffer[position + 1], &buffer[position], buf_len - position + 1);
-							 buffer[position] = c;
-							 shiftright(1);
-							 insertatmiddle = true;
-						 }
-						 position++;
-					 }
-		}
-
-		buf_len = strlen(buffer);
-
-		if (replaced) {
-			shiftleft(buf_len);
-			clearline();
-		}
-
-		if (navigated && buf_len >= 1) {
-			if (position > 0) {
-				shiftleft(position);  // move cursor to the beginning
-			}
-			clearline();
-			position = buf_len;
-		}
-
-		if (moved) {
-			moved = false;
-			continue;
-		}
-
-		if (!navigated && !replaced) {
-			if (position != buf_len) {
-				// not at normal place
-				if (backspaced) {
-					shiftleft(position + 1);
-				} else {
-					shiftleft(position);  // move cursor to the beginning
+				if (c > 31 && c < 127) {
+					if (position == buf_len) {
+						// Append character to the end of the buffer
+						buffer[position] = c;
+						buffer[position + 1] = '\0';
+					} else {
+						// Insert character at the current position
+						memmove(&buffer[position+1], &buffer[position], buf_len - position + 1);
+                        buffer[position] = c;
+					}
+					position++;
 				}
-			} else if (buf_len > 1) {
-				if (backspaced) {
-					shiftleft(buf_len + 1);  // move cursor to the beginning
-				} else {
-					shiftleft(buf_len - 1);  // move cursor to the beginning
-				}
-			} else if (buf_len == 1) {
-				if (backspaced) {
-					shiftleft(2);
-				}
-			} else if (buf_len == 0) {
-				if (backspaced) {
-					shiftleft(1);	
-				}
-			}
-			clearline();
-		} else {
-			navigated = false;
 		}
 
-		highlight(buffer, paths);
-
-		if (backspaced) {
-			if (buf_len != position) {
-				shiftleft(buf_len - position);
-			}
-			backspaced = false;
-		}
-		if (insertatmiddle) {
-			shiftleft(buf_len - position); // move cursor back to where it was
-			insertatmiddle = false;
-		}
-		if (replaced) {
-			replaced = false;
-		}
-
+		render(prompt, buffer, position, &prev_lines, paths);
+		
 		// If we have exceeded the buffer, reallocate.
-		if ((buf_len + 1) >= bufsize) {
+		if ((strlen(buffer) + 1) >= bufsize) {
 			bufsize += RL_BUFSIZE;
 			buffer = realloc(buffer, bufsize);
 			if (!buffer) {
@@ -475,11 +470,11 @@ void command_loop(char **paths)
 		cwd[j] = '\0';
 
 		/* Blue time string, pink time, teal arrow */
-		printf("\033[34m%s\033[m \033[35m[%s] \033[36m>\033[m ", timestr, cwd);
-		fflush(stdout);
+		char prompt[128];
+		snprintf(prompt, 128, "\033[34m%s\033[m \033[35m[%s] \033[36m>\033[m ", timestr, cwd);
 
 		cmd_count = 0; // upward arrow key resets command count
-		line = readline(paths);
+		line = readline(paths, prompt);
 		if (line == NULL) {
 			printf("\n");
 			continue;
